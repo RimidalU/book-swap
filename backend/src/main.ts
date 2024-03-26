310cf90d2d257dad250c89c58081af1594a4b638
RimidalU <stankevichuuladimiru@gmail.com>
2024-03-26 19:47:57 +0300

    feat: add api_port from env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
+
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  const config = await app.get(ConfigService)
+
+  const port = +config.get<number>('API_PORT') || 3000
+
+  await app.listen(port, () =>
+    console.log(`Server is listening on port ${port}`),
+  )
 }
+
 bootstrap()
