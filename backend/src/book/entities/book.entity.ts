18532fdac203401a5b706efac54a64cd2b1ddb9a
RimidalU <stankevichuuladimiru@gmail.com>
2024-03-26 21:02:23 +0300

    feat: implement book entity

---
+++ backend/src/book/entities/book.entity.ts
@@ -0,0 +1,45 @@
+import {AfterUpdate, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm'
+
+@Entity('book')
+export class BookEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  name: string
+
+  @Column()
+  author: string
+
+  @Column('int2', { nullable: true })
+  year?: number
+
+  @Column('text', { nullable: true })
+  description?: string
+
+  @Column('int2')
+  condition: number
+
+  @Column()
+  owner: number
+
+  @Column('boolean', { default: false })
+  isBorrowed: boolean
+
+  @Column( { nullable: true })
+  borrower?: number
+
+  @Column('simple-array')
+  borrowersQueue: number[]
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date
+
+  @AfterUpdate()
+  checkIsBorrowed(){
+    this.borrower ? this.isBorrowed = true : false
+  }
+}
---
+++ backend/src/database/migrations/1711475205960-migrations.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migrations1711475205960 implements MigrationInterface {
+    name = 'Migrations1711475205960'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "book" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "author" character varying NOT NULL, "year" smallint, "description" text, "condition" smallint NOT NULL, "owner" integer NOT NULL, "isBorrowed" boolean NOT NULL DEFAULT false, "borrower" integer, "borrowersQueue" text NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_a3afef72ec8f80e6e5c310b28a4" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "book"`);
+    }
+
+}
