2c144bee856ea469922fe19f61aee3dac18c17f5
RimidalU <stankevichuuladimiru@gmail.com>
2024-03-27 13:38:13 +0300

    test: add create book book-controller test

--- backend/src/book/book.controller.spec.ts
+++ backend/src/book/book.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { BookController } from './book.controller'
 import { BookService } from './book.service'
+import { bookItem, newItemInfo } from './mocks'
 
 describe('BookController', () => {
   let controller: BookController
@@ -12,13 +13,15 @@ describe('BookController', () => {
       providers: [
         {
           provide: BookService,
-          useValue: {},
+          useValue: {
+            create: jest.fn().mockReturnValue(bookItem.id),
+          },
         },
       ],
     }).compile()
 
-    service = moduleRef.get<BookService>(BookService)
-    controller = moduleRef.get<BookController>(BookController)
+    service = moduleRef.get(BookService)
+    controller = moduleRef.get(BookController)
   })
 
   it('controller should be defined', () => {
@@ -27,5 +30,15 @@ describe('BookController', () => {
 
   it('service should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  describe('create book method', () => {
+    it('check the book created', async () => {
+      expect(await controller.create(newItemInfo)).toBe(bookItem.id)
+
+      expect(service.create).toHaveBeenCalledWith({
+        ...newItemInfo,
+      })
+    })
   })
 })
