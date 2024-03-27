4df8acc3152bf7ce677d2dcbdc5ddda6157b7067
RimidalU <stankevichuuladimiru@gmail.com>
2024-03-27 13:38:35 +0300

    feat: add create book method in book-controller

--- backend/src/book/book.controller.ts
+++ backend/src/book/book.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
 
 import { BookService } from './book.service'
 
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
+
+  @Post()
+  async create(@Body() payload: any): Promise<number> {
+    return await this.bookService.create(payload)
+  }
 }
