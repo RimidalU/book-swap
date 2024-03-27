72578aeebcf2731ea956c9272733af76766983db
RimidalU <stankevichuuladimiru@gmail.com>
2024-03-28 00:23:49 +0300

    feat: implement get-all book controller method

--- backend/src/book/book.controller.ts
+++ backend/src/book/book.controller.ts
@@ -1,11 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post } from '@nestjs/common'
 
 import { BookService } from '@src/book/book.service'
+
 import { CreateBookDto } from '@src/book/dto'
+import { BookEntity } from '@src/book/entities'
 
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
+
+  @Get()
+  async getAll(): Promise<BookEntity[]> {
+    return await this.bookService.getAll()
+  }
 
   @Post()
   async create(@Body() payload: CreateBookDto): Promise<number> {
