6054bbd48b48c993b0457f634df1e160b3551114
RimidalU <stankevichuuladimiru@gmail.com>
2024-03-27 13:18:52 +0300

    feat: add create book method in book-service

--- backend/src/book/book.service.ts
+++ backend/src/book/book.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common'
-import { Repository } from 'typeorm'
-import { BookEntity } from './entities'
 import { InjectRepository } from '@nestjs/typeorm'
+
+import { Repository } from 'typeorm'
+
+import { BookEntity } from './entities'
+import { ownerMockId } from './mocks'
 
 @Injectable()
 export class BookService {
@@ -9,4 +12,14 @@ export class BookService {
     @InjectRepository(BookEntity)
     private readonly bookRepository: Repository<BookEntity>,
   ) {}
+  async create(payload: any): Promise<number> {
+    const ownerId = ownerMockId
+    payload.owner = ownerId
+
+    const newBook = new BookEntity()
+    Object.assign(newBook, payload)
+
+    const book = await this.bookRepository.save(newBook)
+    return book.id
+  }
 }
