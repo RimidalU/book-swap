8c4fc6d491b52a398566cf35051dc8d338056d8c
RimidalU <stankevichuuladimiru@gmail.com>
2024-03-29 00:18:43 +0300

    feat: implement user mock

---
+++ backend/src/user/mocks/user-item.mock.ts
@@ -0,0 +1,21 @@
+import {UserType} from "@src/user/types";
+
+export const userItem: UserType = {
+  id: 11,
+  name: 'User Name',
+  bio: 'User Name bio',
+  avatar: 'Link to avatar',
+  email: 'user@email.com',
+  password: '$2a$10$e4O2ybUAsEfQfFiW8r1Ag.00kFv9a/4ZdUZbgrwSjsR7FBzMmHNpO',
+  token: null,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}
+export const ownerMockId = 123
+
+export const newItemInfo = {
+  name: userItem.name,
+  bio: userItem.bio,
+  email: userItem.email,
+  password: userItem.password,
+}
