ba11c35c5f257abfc7bbf6d9e31bbb2a8b88e838
RimidalU <stankevichuuladimiru@gmail.com>
2024-03-28 20:14:59 +0300

    test: add service and db to tests

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing'
+
 import { UserController } from './user.controller'
+import {UserService} from "@src/user/user.service";
+
 
 describe('UserController', () => {
   let controller: UserController
+  let service: UserService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {},
+        },
+      ],
     }).compile()
 
-    controller = module.get<UserController>(UserController)
+    controller = module.get(UserController)
+    service = module.get(UserService)
   })
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  it('service should be defined', () => {
+    expect(service).toBeDefined()
   })
 })
--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import {getRepositoryToken} from "@nestjs/typeorm";
+
 import { UserService } from './user.service'
+import {Repository} from "typeorm";
+
+import {UserEntity} from "@src/user/entities";
 
 describe('UserService', () => {
   let service: UserService
+let userRepository: Repository<UserEntity>
+
+  const USER_REPOSITORY_TOKEN = getRepositoryToken(UserEntity)
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserService,
+        {
+          provide: USER_REPOSITORY_TOKEN,
+          useValue: {},
+        },
+      ],
     }).compile()
 
     service = module.get(UserService)
+    userRepository = module.get(USER_REPOSITORY_TOKEN)
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('should bookRepository be defined', () => {
+    expect(userRepository).toBeDefined()
   })
 })
