9afbbe9f2d9ca2879a29dff7220b5c4fa175a14c
RimidalU <stankevichuuladimiru@gmail.com>
2024-03-29 00:37:44 +0300

    feat: implement update-user dto

---
+++ backend/src/user/dto/update-user.dto.ts
@@ -0,0 +1,31 @@
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+} from 'class-validator'
+
+export class UpdateUserDto {
+  @IsString()
+  @IsOptional()
+  readonly name: string
+
+  @IsEmail()
+  @IsOptional()
+  readonly email: string
+
+  @IsString()
+  @IsOptional()
+  readonly password: string
+
+  @IsString()
+  @IsOptional()
+  readonly bio?: string
+
+  @IsString()
+  @IsOptional()
+  readonly avatar?: string
+
+  @IsString()
+  @IsOptional()
+  readonly token?: string
+}
