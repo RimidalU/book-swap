a7c5c142c19da2026e7e6e502e434f2d0bd77c4a
RimidalU <stankevichuuladimiru@gmail.com>
2024-03-29 00:36:52 +0300

    feat: implement create-user dto

---
+++ backend/src/user/dto/create-user.dto.ts
@@ -0,0 +1,24 @@
+import {IsEmail, IsNotEmpty, IsString} from 'class-validator'
+import { IsOptional } from 'class-validator'
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string
+
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string
+
+  @IsString()
+  @IsNotEmpty()
+  readonly password: string
+
+  @IsString()
+  @IsOptional()
+  readonly bio?: string
+
+  @IsString()
+  @IsOptional()
+  readonly avatar?: string
+}
