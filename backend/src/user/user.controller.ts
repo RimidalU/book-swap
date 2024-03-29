2bcd8cb7be169eefc6173c3086731e1664fa03fa
RimidalU <stankevichuuladimiru@gmail.com>
2024-03-29 21:13:33 +0300

    feat: implement user controller crud methods

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common'
+
+import { UserService } from '@src/user/user.service'
+
+import { UserEntity } from '@src/user/entities'
+import { CreateUserDto, UpdateUserDto } from '@src/user/dto'
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAll(): Promise<UserEntity[]> {
+    return await this.userService.getAll()
+  }
+
+  @Get(':id')
+  async getById(@Param('id', ParseIntPipe) id: number): Promise<UserEntity> {
+    return await this.userService.getById(id)
+  }
+
+  @Post()
+  async create(@Body() payload: CreateUserDto): Promise<number> {
+    return await this.userService.create(payload)
+  }
+
+  @Delete(':id')
+  async remove(@Param('id', ParseIntPipe) id: number): Promise<number> {
+    return await this.userService.remove(id)
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateUserDto,
+  ): Promise<number> {
+    return await this.userService.update(id, payload)
+  }
+}
