e2899ce78873a66fd149448e022500f114372573
RimidalU <stankevichuuladimiru@gmail.com>
2024-04-03 21:39:46 +0300

    feat: add base swagger description to app routes

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
 
 import { AppService } from '@src/app.service'
 
+@ApiTags('App routes')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
