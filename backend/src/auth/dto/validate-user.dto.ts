38ad1b2ea2706f6d1bab4224694e486e63b2b59c
RimidalU <stankevichuuladimiru@gmail.com>
2024-04-03 21:40:39 +0300

    feat: add swagger description to auth routes

--- backend/src/auth/dto/validate-user.dto.ts
+++ backend/src/auth/dto/validate-user.dto.ts
@@ -1,11 +1,20 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class ValidateUserDto {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'don-dou@email.com',
+    description: 'User Email',
+  })
   readonly email: string
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'Don-Dou#Password91',
+    description: 'Strong Password',
+  })
   readonly password: string
 }
