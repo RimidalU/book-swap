4c16e3f973e742dc961b40789c322c0492301857
RimidalU <stankevichuuladimiru@gmail.com>
2024-04-03 21:52:26 +0300

    style: lint fix

--- backend/src/app.swagger.ts
+++ backend/src/app.swagger.ts
@@ -1,7 +1,4 @@
-import {
-  DocumentBuilder,
-  SwaggerModule,
-} from '@nestjs/swagger'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { INestApplication } from '@nestjs/common'
 
 export const initSwagger = (app: INestApplication) => {
