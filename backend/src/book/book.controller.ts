7c2fa983207a7ab4225f788e5f4808ce1b793075
RimidalU <stankevichuuladimiru@gmail.com>
2024-04-04 15:58:22 +0300

    feat: add build-response methods to book controller

--- backend/src/book/book.controller.ts
+++ backend/src/book/book.controller.ts
@@ -13,18 +13,34 @@ import {
 
 import { BookService } from '@src/book/book.service'
 
-import { CreateBookDto, UpdateBookDto } from '@src/book/dto'
+import {
+  BookConfirmationResponseDto,
+  BookItemDto,
+  CreateBookDto,
+  UpdateBookDto,
+} from '@src/book/dto'
 import { BookEntity } from '@src/book/entities'
 import { JwtAuthGuard } from '@src/auth/jwt-auth.guard'
 import { UserInfo } from '@src/user/decorators/user-info.decorator'
 import { BooksResponseInterface } from '@src/book/types/books-response.interface'
 import { QueryInterface } from '@src/book/types'
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse
+} from '@nestjs/swagger'
+import { ShortBookItemDto } from '@src/book/dto'
 
+@ApiTags('Book routes')
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @Get()
   async getAll(
     @UserInfo('id') currentUserId: number,
@@ -38,13 +54,23 @@ export class BookController {
     return await this.bookService.getById(id)
   }
 
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   @Post()
+  @ApiOperation({ summary: 'Create Book' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiCreatedResponse({
+    description: 'Book created',
+    type: BookConfirmationResponseDto,
+  })
   async create(
     @UserInfo('id') currentUserId: number,
     @Body() payload: CreateBookDto,
-  ): Promise<number> {
-    return await this.bookService.create(currentUserId, payload)
+  ): Promise<BookConfirmationResponseDto> {
+    const bookId =  await this.bookService.create(currentUserId, payload)
+
+    return this.buildBookConfirmationResponse((bookId))
   }
 
   @Delete(':id')
@@ -80,5 +106,49 @@ export class BookController {
     @Param('id', ParseIntPipe) bookId: number,
   ): Promise<number> {
     return await this.bookService.removeFromFavorites(currentUserId, bookId)
+  }
+
+  private buildShortBookResponse(
+    book: BookEntity,
+    inFavorites = false,
+  ): ShortBookItemDto {
+    return {
+      itemId: book.id,
+      item: {
+        name: book.name,
+        author: book.author,
+        year: book.year,
+        description: book.description,
+        condition: book.condition,
+        likes: book.likes,
+        ownerId: book.owner.id,
+        ownerAvatar: book.owner.avatar,
+        inFavorites: inFavorites,
+      },
+    }
+  }
+
+  private buildBookResponse(book: BookEntity): BookItemDto {
+    return {
+      ...this.buildShortBookResponse(book),
+      itemStatus: {
+        isBorrowed: book.isBorrowed,
+        borrower: {
+          borrowerName: 'book.borrower.name',
+          borrowerId: 11,
+        },
+        // borrowersQueue: book.borrowersQueue,  TODO: add borrowersQueue
+      },
+    }
+  }
+
+  private buildBookConfirmationResponse(
+    bookId: number,
+  ): BookConfirmationResponseDto {
+    return {
+      book: {
+        itemId: bookId,
+      },
+    }
   }
 }
