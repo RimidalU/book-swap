010423e163d2ec0dc359819cf88acf939d7c9bc5
RimidalU <stankevichuuladimiru@gmail.com>
2024-04-05 18:52:28 +0300

    feat: implement tag entity

---
+++ backend/src/tag/entities/tag.entity.ts
@@ -0,0 +1,20 @@
+import {BeforeInsert, Column, Entity, PrimaryGeneratedColumn} from 'typeorm'
+import {ApiProperty} from "@nestjs/swagger";
+
+@Entity('tag')
+export class TagEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  @ApiProperty({
+    example: 'adventures',
+    description: 'Tag Name',
+  })
+  name: string
+
+@BeforeInsert()
+  convertToLowerCase(){
+    this.name = this.name.toLowerCase()
+}
+}
