a6a03f9a6d79da6c201b111f7b3864bf4c2ff8d8
RimidalU <stankevichuuladimiru@gmail.com>
2024-04-06 01:19:27 +0300

    add tag dtos

--- backend/src/tag/dto/index.ts
+++ backend/src/tag/dto/index.ts
@@ -1,2 +1,5 @@
 export * from './tag-confirmation-response.dto'
 export * from './create-tag.dto'
+export * from './tag-item.dto'
+export * from './tag-response.dto'
+export * from './tags-response.dto'
