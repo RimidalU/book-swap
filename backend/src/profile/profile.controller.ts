b4535eb88d64baeb13aa9a831af1899bafadd9e1
RimidalU <stankevichuuladimiru@gmail.com>
2024-04-10 00:20:50 +0300

    style: implement get-profile swagger decorator

--- backend/src/profile/profile.controller.ts
+++ backend/src/profile/profile.controller.ts
@@ -1,35 +1,23 @@
 import { Controller, Get, Param, ParseIntPipe, UseGuards } from '@nestjs/common'
-import {
-  ApiBearerAuth,
-  ApiNotFoundResponse,
-  ApiOperation,
-  ApiResponse,
-  ApiTags,
-  ApiUnauthorizedResponse,
-} from '@nestjs/swagger'
+import { ApiTags } from '@nestjs/swagger'
+
 import { JwtAuthGuard } from '@src/auth/jwt-auth.guard'
 import { UserInfo } from '@src/user/decorators/user-info.decorator'
+import { ProfileService } from '@src/profile/profile.service'
+import { GetProfileSwaggerDecorator } from '@src/profile/decorators'
+
 import { ProfileResponseDto } from '@src/profile/dto'
 import { ProfileType } from '@src/profile/types'
 import { ProfileItemDto } from '@src/profile/dto/profile-item.dto'
-import { ProfileService } from '@src/profile/profile.service'
 
 @Controller('profile')
 @ApiTags('Profile routes')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
-  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   @Get(':id')
-  @ApiOperation({ summary: 'Get Profile by id' })
-  @ApiNotFoundResponse({ description: 'Not Found' })
-  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
-  @ApiResponse({
-    status: 200,
-    description: 'The found record',
-    type: ProfileResponseDto,
-  })
+  @GetProfileSwaggerDecorator()
   async getProfile(
     @Param('id', ParseIntPipe) id: number,
     @UserInfo('id') currentUserId: number,
