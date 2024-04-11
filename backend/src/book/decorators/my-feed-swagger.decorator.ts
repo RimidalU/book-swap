818727b925e6e720834606b6a6c582879097a97d
RimidalU <stankevichuuladimiru@gmail.com>
2024-04-12 01:07:08 +0300

    style: implement my-feed-swagger decorator

---
+++ backend/src/book/decorators/my-feed-swagger.decorator.ts
@@ -0,0 +1,47 @@
+import { applyDecorators } from '@nestjs/common'
+
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiQuery,
+  ApiResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger'
+
+import { BooksResponseDto } from '@src/book/dto'
+
+export function MyFeedSwaggerDecorator() {
+  return applyDecorators(
+    ApiBearerAuth(),
+
+    ApiOperation({
+      summary: 'Get All Books of users from my subscriptions list',
+    }),
+    ApiUnauthorizedResponse({ description: 'Unauthorized' }),
+    ApiResponse({
+      status: 200,
+      description: 'The found records',
+      type: BooksResponseDto,
+    }),
+
+    ApiQuery({ name: 'limit', required: false, description: 'Items on page' }),
+    ApiQuery({
+      name: 'offset',
+      required: false,
+      description: 'Offset on page',
+    }),
+    // ApiQuery({
+    //   name: 'owner',
+    //   required: false,
+    //   description: 'Owner name(complete match)',
+    // }),
+    // ApiQuery({ name: 'name', required: false, description: 'Book name' }),
+    // ApiQuery({ name: 'author', required: false, description: 'Author name' }),
+    // ApiQuery({ name: 'tag', required: false, description: 'Tags of Book' }),
+    // ApiQuery({
+    //   name: 'selectedUser',
+    //   required: false,
+    //   description: "Books in this user's favorites",
+    // }),
+  )
+}
