2f875e52e876d1c13e1158c9e9e4ed479f019024
RimidalU <stankevichuuladimiru@gmail.com>
2024-04-13 09:28:10 +0300

    refactor: add get-typeorm-module-config function

--- backend/src/configs/index.ts
+++ backend/src/configs/index.ts
@@ -1,1 +1,2 @@
 export * from './get-config-module.config'
+export * from './get-typeorm-module.config'
--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,26 +1,14 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { getTypeormModuleConfig } from '@src/configs/get-typeorm-module.config'
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-
-      useFactory: async (configService: ConfigService) => ({
-        type: configService.getOrThrow<'postgres'>('TYPEORM_CONNECTION'),
-        username: configService.getOrThrow<string>('TYPEORM_USER'),
-        password: configService.getOrThrow<string>('TYPEORM_PASSWORD'),
-        database: configService.getOrThrow<string>('TYPEORM_DATABASE'),
-        port: configService.getOrThrow<number>('TYPEORM_PORT'),
-        host: configService.getOrThrow<string>('TYPEORM_HOST'),
-        entities: [__dirname + '../../**/*.entity{.ts,.js}'],
-        migrations: [__dirname + '/migrations/**/*{.ts,js}'],
-        synchronize: false,
-        autoLoadEntities: true,
-        useUTC: true,
-      }),
+      useFactory: getTypeormModuleConfig,
     }),
   ],
 })
