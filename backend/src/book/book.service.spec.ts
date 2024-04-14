df3e5bba2173a4d020b0d7e3f7d120af9e59dce4
RimidalU <stankevichuuladimiru@gmail.com>
2024-04-14 15:45:24 +0300

    test: refactor book service test

--- backend/src/book/book.service.spec.ts
+++ backend/src/book/book.service.spec.ts
@@ -1,23 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { getRepositoryToken } from '@nestjs/typeorm'
-import { Repository } from 'typeorm'
+import { DataSource, Repository } from 'typeorm'
 
 import { BookService } from './book.service'
 
 import { BookEntity } from './entities'
+import { UserEntity } from '@src/user/entities'
 
-import { bookItem, newItemInfo, ownerMockId } from './mocks'
+import { bookItem, newItemInfo } from './mocks'
 import { BookNotFoundException } from './exceptions'
+import { userItem } from '@src/user/mocks'
 
 describe('BookService', () => {
   let service: BookService
+
   let bookRepository: Repository<BookEntity>
   const newBookInfo = {
     description: 'New description',
     year: 1990,
   }
+  const currentUserId = userItem.id
 
   const BOOK_REPOSITORY_TOKEN = getRepositoryToken(BookEntity)
+  const USER_REPOSITORY_TOKEN = getRepositoryToken(UserEntity)
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -32,6 +37,33 @@ describe('BookService', () => {
             remove: jest.fn().mockReturnValue(bookItem),
           },
         },
+        // UserService,
+        {
+          provide: USER_REPOSITORY_TOKEN,
+          useValue: {
+            findOne: jest.fn().mockReturnValue(userItem),
+            findOneBy: jest
+              .fn()
+              .mockReturnValue({ ...userItem, favorites: [] }),
+          },
+        },
+        {
+          provide: DataSource,
+          useValue: {
+            getRepository: jest.fn().mockImplementation(() => ({
+              createQueryBuilder: jest.fn().mockImplementation(() => ({
+                leftJoinAndSelect: jest.fn().mockImplementation(() => ({
+                  orderBy: jest.fn().mockReturnValue([bookItem]),
+                  limit: jest.fn().mockReturnValue([bookItem]),
+                  offset: jest.fn().mockReturnValue([bookItem]),
+                  getCount: jest.fn().mockReturnValue(0),
+
+                  getMany: jest.fn().mockReturnValue([bookItem]),
+                })),
+              })),
+            })),
+          },
+        },
       ],
     }).compile()
 
@@ -39,41 +71,39 @@ describe('BookService', () => {
     bookRepository = module.get(BOOK_REPOSITORY_TOKEN)
   })
 
-  it('should be defined', () => {
+  it('service should be defined', () => {
     expect(service).toBeDefined()
   })
 
-  it('should bookRepository be defined', () => {
+  it('bookRepository should bookRepository be defined', () => {
     expect(bookRepository).toBeDefined()
   })
 
   describe('create book method', () => {
     it('check the book created', async () => {
-      expect(await service.create(newItemInfo)).toBe(bookItem.id)
+      expect(await service.create(currentUserId, newItemInfo)).toBe(bookItem.id)
       expect(bookRepository.save).toHaveBeenCalledWith({
         ...newItemInfo,
-        owner: ownerMockId,
+        owner: userItem,
       })
     })
   })
 
   describe('getAll books method', () => {
     it('the array of books should be returned', async () => {
-      expect(await service.getAll()).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({
-            ...bookItem,
-          }),
-        ]),
-      )
-
-      expect(bookRepository.find).toHaveBeenCalledWith()
+      expect(await service.getAll(currentUserId, {})).toEqual({
+        books: [{ ...bookItem, inFavorites: false }],
+        count: 0,
+      })
     })
   })
 
   describe('getById method', () => {
     it('the book with correct id should be returned', async () => {
-      expect(await service.getById(bookItem.id)).toEqual(bookItem)
+      expect(await service.getById(currentUserId, bookItem.id)).toEqual({
+        ...bookItem,
+        inFavorites: false,
+      })
 
       expect(bookRepository.findOneBy).toHaveBeenCalledWith({
         id: bookItem.id,
@@ -83,15 +113,19 @@ describe('BookService', () => {
     it('getById book with wrong id should throw an exception', async () => {
       bookRepository.findOneBy = jest.fn().mockReturnValue(undefined)
 
-      await expect(service.getById(bookItem.id)).rejects.toThrowError(
-        BookNotFoundException,
-      )
+      await expect(
+        service.getById(currentUserId, bookItem.id),
+      ).rejects.toThrowError(BookNotFoundException)
     })
   })
 
   describe('remove method', () => {
     it('remove the book with correct id should be returned book id', async () => {
-      expect(await service.remove(bookItem.id)).toEqual(bookItem.id)
+      service.checkPermission = jest.fn().mockReturnValue(true)
+
+      expect(await service.remove(currentUserId, bookItem.id)).toEqual(
+        bookItem.id,
+      )
 
       expect(await bookRepository.findOneBy).toHaveBeenCalledWith({
         id: bookItem.id,
@@ -102,17 +136,19 @@ describe('BookService', () => {
     it('remove book with wrong id should throw an exception', async () => {
       bookRepository.findOneBy = jest.fn().mockReturnValue(undefined)
 
-      await expect(service.remove(bookItem.id)).rejects.toThrowError(
-        BookNotFoundException,
-      )
+      await expect(
+        service.remove(currentUserId, bookItem.id),
+      ).rejects.toThrowError(BookNotFoundException)
     })
   })
 
   describe('update method', () => {
     it('update the book with correct id should be returned book id', async () => {
-      expect(await service.update(bookItem.id, newBookInfo)).toEqual(
-        bookItem.id,
-      )
+      service.checkPermission = jest.fn().mockReturnValue(true)
+
+      expect(
+        await service.update(currentUserId, bookItem.id, newBookInfo),
+      ).toEqual(bookItem.id)
 
       expect(await bookRepository.findOneBy).toHaveBeenCalledWith({
         id: bookItem.id,
@@ -128,7 +164,7 @@ describe('BookService', () => {
       bookRepository.findOneBy = jest.fn().mockReturnValue(undefined)
 
       await expect(
-        service.update(bookItem.id, newBookInfo),
+        service.update(currentUserId, bookItem.id, newBookInfo),
       ).rejects.toThrowError(BookNotFoundException)
     })
   })
