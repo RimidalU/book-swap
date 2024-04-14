7e6900d296f32c2e0befd387ce97fd758c7d7a91
RimidalU <stankevichuuladimiru@gmail.com>
2024-04-14 12:10:08 +0300

    feat: implement http-extended-exception filter

---
+++ backend/src/filters/HttpExtendedExceptionFilter.ts
@@ -0,0 +1,31 @@
+import {
+  ExceptionFilter,
+  ArgumentsHost,
+  Catch,
+  HttpException,
+} from '@nestjs/common'
+import { Response } from 'express'
+import { ExpressRequestType } from '@src/user/types'
+
+@Catch(HttpException)
+export class HttpExtendedExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse<Response>()
+    const request = ctx.getRequest<ExpressRequestType>()
+    const status = exception.getStatus()
+
+    response.status(status).json({
+      exception: {
+        statusCode: status,
+        message: exception.message,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        userInfo: {
+          id: request.user.id,
+        },
+        payload: request.body,
+      },
+    })
+  }
+}
