43c661468e7dc9811d21c763b6c3153381ab714c
RimidalU <stankevichuuladimiru@gmail.com>
2024-04-19 10:21:13 +0300

    test: update file service tests

--- backend/src/file/file.service.spec.ts
+++ backend/src/file/file.service.spec.ts
@@ -1,20 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { FileService } from './file.service'
 
+import {fileMock} from "@src/file/mocks";
+
 describe('FileService', () => {
   let service: FileService
   const currentUserId = 1
-  const file = {
-    fieldname: 'file',
-    encoding: '7bit',
-    originalname: 'file.jpeg',
-    mimetype: 'image/jpeg',
-    path: 'something',
-    buffer: Buffer.from('Buffer'),
-    size: 51828,
-    filename: 'file-name',
-    destination: 'destination-path',
-  } as Express.Multer.File
+  const filesSet = {
+    origin: fileMock,
+    webp: fileMock,
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -30,19 +25,18 @@ describe('FileService', () => {
 
   describe('save-files method', () => {
     it('check returned files info with current request', async () => {
-      expect(
-        await service.saveFiles(currentUserId, [
-          {
-            originalname: file.originalname,
-            buffer: file.buffer,
-          },
-        ]),
-      ).toEqual([
-        {
-          name: file.originalname,
-          url: `img/${file.originalname}`,
+      expect(await service.saveImg(currentUserId, filesSet)).toEqual({
+        origin: {
+          name: fileMock.originalname,
+          type: 'jpeg',
+          url: 'img/file-name',
         },
-      ])
+        webp: {
+          name: fileMock.originalname,
+          type: 'jpeg',
+          url: 'img/file-name',
+        },
+      })
     })
   })
 })
